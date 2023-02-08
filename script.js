27ef5be4b5e10749912b2ff55eb770a96f56c9da
calvinyoon <calvinxproject@gmail.com>
2023-02-08 13:03:26 -0500

    added content

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
-function removeTransition(e) {
-    if (e.propertyName !== 'transform') return;
-    e.target.classList.remove('playing');
-}
-
 function playSound(e) {
+  //input is e which has keyCode.
+
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  //access the audio attribute -> audio[ ]
-  //attribute is data-key = " "
-  //data-key is ${e.keyCode}
-  //ie: audio[data-key="45"]
+  //we want to access audio element that has corresponding keyCode as attribute.
+  //ie: access element with audio[data-key="45"]
+    //attribute is data-key = " "
+    //data-key is ${e.keyCode}
   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-  //access the div attribute -> div[ ]
+  //access the div element that has corresponding keyCode as attribute
+  //ie: access element with div[data-key="45"]
+
   if (!audio) return;
   //if audio is false, dont execute function
 
   key.classList.add('playing');
-  //if audio is true, add .playing
+  //else, add .playing to key element
   audio.currentTime = 0;
-  //return time to 0
+  //rewind to start
   audio.play();
   //play audio
 }
 
+function removeTransition(e) {
+    //event here is transitionend 
+    if (e.propertyName !== 'transform') return;
+    //arbitrarily selected transform but usually should be longest event
+    this.classList.remove('playing');
+    //this here is key bc its object that got called in transitionend listener
+}
+
 const keys = Array.from(document.querySelectorAll('.key'));
+
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+//listen on each key for which transitionend happened
+//cannot listen to all node elements simultaneously - need forEach
+
 window.addEventListener('keydown', playSound);
